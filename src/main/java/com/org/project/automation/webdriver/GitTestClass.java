8fd71e33efb367443affcbc7584c0b9ad7314635
Lamarnika <Lamarnika1234.gmail.com>
2025-10-17 12:55:01 +0530

    adds new java file to print message

---
+++ src/main/java/com/org/project/automation/webdriver/GitTestClass.java
@@ -0,0 +1,9 @@
+package com.org.project.automation.webdriver;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class GitTestClass {
+    public static void main(String[] args) {
+        System.out.println("Hello github");
+    }
+}
