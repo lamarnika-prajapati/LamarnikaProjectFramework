38f303e53912b95c257ff5b5c3bb695d0537a325
lamarnika-prajapati <pr.lamarnika@gmail.com>
2025-10-17 17:07:08 +0530

    Update NewGithubFile2.java

--- src/main/java/com/org/project/automation/webdriver/NewGithubFile2.java
+++ src/main/java/com/org/project/automation/webdriver/NewGithubFile2.java
@@ -3,6 +3,6 @@ package com.org.project.automation.webdriver;
 public class NewGithubFile2 {
     public static void main(String[] args) {
         String name="Prajapati";
-
+       System.out.println(name);
     }
 }
